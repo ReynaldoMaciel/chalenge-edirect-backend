a7c2c47403c4ca09d2a4d20d5ae7d298ffc7ac9c
Reynaldo <reynaldommaciel@gmail.com>
2019-08-04 20:25:32 -0300

    add done parameter

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -8,6 +8,7 @@ const getAll = async (idProject, idUser) => knex.from('task')
 
 const create = async ({ description, idProject }, idUser) => {
   let projectInfo = await knex.from('project').where({ idProject }).first()
+  if (!projectInfo) throw { message: 'Project not found', statusCode: 400 }
   if (projectInfo.idUser !== idUser) throw { message: 'Cannot create a task for another user project', statusCode: 401 }
   return task.query().insertAndFetch({
     idProject,
@@ -16,13 +17,19 @@ const create = async ({ description, idProject }, idUser) => {
   })
 }
 
-const update = async ({ idTask, description }, idUser) => {
+const update = async ({ idTask, done, description }, idUser) => {
   let taskInfo = await knex.from('task').innerJoin('project', { 'task.idProject': 'project.idProject' }).where({ idTask }).first()
   if (!taskInfo) throw { message: 'Task not found', statusCode: 400 }
   if (taskInfo.finishDate) throw { message: 'Tasks finished can not be updated', statusCode: 401 }
   if (taskInfo.idUser !== idUser) throw { message: 'Tasks of others users can not be updated', statusCode: 401 }
 
-  return task.query().updateAndFetchById(idTask, { description })
+  return task.query().updateAndFetchById(
+    idTask,
+    {
+      description,
+      finishDate: done ? new Date().toISOString() : null,
+    },
+  )
 }
 
 const remove = async (idTask, idUser) => {
